3622945bdafbcb7b4ce7e747d9c37dd8ae675b04
Duho Wise <Duhowise@gmail.com>
2019-08-28 14:30:02 +0000

    Add Payment order class

---
+++ EntityFrameworkCore Migrations/PaymentOrder.cs
@@ -0,0 +1,23 @@
+﻿namespace EntityFrameworkCore_Migrations
+{
+    public class PaymentOrder
+    {
+        public PaymentOrder(double amount, string phone, string payee, string beneficiary, string poNumber)
+        {
+            Amount = amount;
+            Phone = phone;
+            Payee = payee;
+            Beneficiary = beneficiary;
+            PoNumber = poNumber;
+        }
+        public double Amount { get; protected set; }
+
+        public string Phone { get; protected set; }
+
+        public string Payee { get; protected set; }
+
+        public string Beneficiary { get; protected set; }
+
+        public string PoNumber { get; protected set; }
+    }
+}
