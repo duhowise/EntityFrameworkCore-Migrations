ef6f30f48e0b84535211090e0eb2a47866533ca1
Duho Wise <Duhowise@gmail.com>
2019-08-28 14:21:53 +0000

    Added DbContext

---
+++ EntityFrameworkCore Migrations/EntityMigrationsContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCore_Migrations
+{
+    public class EntityMigrationsContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer("Server=.; Database=EntityMigrationsDemo;Trusted_Connection=True;");
+        }
+
+    }
+}
