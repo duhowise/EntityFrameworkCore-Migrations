f5edfaed9b7b60d5b16e16554090f4cb3874dda9
Duho Wise <Duhowise@gmail.com>
2019-08-28 12:59:20 +0000

    Add project files.

---
+++ EntityFrameworkCore Migrations/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCore_Migrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class EntityMigrationsContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer("Server=.; Database=EntityMigrationsDemo;Trusted_Connection=True;");
+        }
+
+    }
+}
